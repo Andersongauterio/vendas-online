e926dc3c15be711de319eaa3b169ab7b034b5bc5
Andersongauterio <anderson.camara.gauterio@gmail.com>
2023-05-28 21:37:49 -0300

    feat: userController

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { User } from './interface/user.interface';
+import { UserService } from './user.service.';
+import { CreateUserDto } from './dto/createUser.dto';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 
 @Controller('user')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async createUser(@Body() createUser: CreateUserDto): Promise<User> {
+    return this.userService.createUser(createUser);
+  }
+
   @Get()
-  async getAllUsers() {
-    return JSON.stringify({ test: 'abc' });
+  async getAllUser(): Promise<User[]> {
+    return this.userService.getAllUser();
   }
 }
